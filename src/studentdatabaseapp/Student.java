2efee0f151d37ac8efcb46887024b8a4ed722eb0
tankengr <tankengr@3il.fr>
2022-12-15 22:18:43 +0100

    modification od Student Class

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,7 +1,30 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class Student {
 
+    // Properties
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentID;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCourse = 600;
 
+    // Constructor
+    public Student() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter student first name : ");
+        this.firstName = in.nextLine();
 
+        System.out.print("Enter student last name : ");
+        this.lastName = in.nextLine();
+
+        System.out.print("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student class level : ");
+        this.gradeYear = in.nextLine();
+
+        System.out.print(firstName + " " + lastName + " " + gradeYear);
+    }
 }
--- src/studentdatabaseapp/StudentDataBaseApp.java
+++ src/studentdatabaseapp/StudentDataBaseApp.java
@@ -2,8 +2,12 @@ package studentdatabaseapp;
 
 public class StudentDataBaseApp {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        // Creation of a new student
+        Student student = new Student();
+        
         // Ask how many new users we want to add
 
+        // Create n number of new students
     }
 }
